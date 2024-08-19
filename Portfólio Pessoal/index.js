69f354dd22413303b076bf9dfe22ad7943ede1b4
William Wieser <166855277+WilliamWieserCoder@users.noreply.github.com>
2024-08-19 14:58:56 -0300

    Add files via upload

---
+++ Portfólio Pessoal/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('downloadCV').addEventListener('click', function() {
+        window.location.href = 'curriculo.pdf';
+    });
+});
