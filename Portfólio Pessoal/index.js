bbc821b92aa4096ea968a732f51f0d9214918345
William Wieser <Willkurrle@gmail.com>
2024-08-20 15:03:17 -0300

    aaa

--- Portfólio Pessoal/index.js
+++ Portfólio Pessoal/index.js
@@ -1,5 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('downloadCV').addEventListener('click', function() {
-        window.location.href = 'curriculo.pdf';
-    });
+
 });
