ffd6777e9952e6870186e01f2064beca5092ebc6
William Wieser <166855277+WilliamWieserCoder@users.noreply.github.com>
2024-08-20 12:47:08 -0300

    Delete Portfólio Pessoal directory

--- Portfólio Pessoal/index.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('downloadCV').addEventListener('click', function() {
-        window.location.href = 'curriculo.pdf';
-    });
-});
